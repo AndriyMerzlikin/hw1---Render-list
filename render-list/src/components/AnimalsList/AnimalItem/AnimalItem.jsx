b8806cb6b52949da0d95b8a532ad99b69d03e6ed
AndriyMerzlikin <lastheroofme@gmail.com>
2024-06-20 14:48:57 +0300

    Update AnimalItem.jsx

--- render-list/src/components/AnimalsList/AnimalItem/AnimalItem.jsx
+++ render-list/src/components/AnimalsList/AnimalItem/AnimalItem.jsx
@@ -5,19 +5,10 @@ import ButtonComponent from "./ButtonComponent/ButtonComponent";
 const AnimalItem = ({
   icon,
   type,
-
   isButtonActive,
   handleDelete,
   handleToggle,
 }) => {
-  //   const [isButtonActive, setIsButtonActive] = useState(false);
-  //   const [buttonName, setButtonName] = useState("Active");
-
-  //   const handleButtonToggle = () => {
-  //     setIsButtonActive(!isButtonActive);
-  //     setButtonName(isButtonActive ? "Active" : "Deactivate");
-  //   };
-
   return (
     <li>
       {icon}{" "}
